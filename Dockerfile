7082df3101bf154b1eb659132919393c09bbbd2b
Angelos-Thefilos Pantelaios <angelost@csd.auth.gr>
2025-02-18 12:04:22 +0200

    FrontEnd get inputs

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ WORKDIR /app
 COPY requirements.txt .
 COPY FrontEnd/ /app/
 
-RUN python -m venv /opt/venv
-ENV PATH="/opt/venv/bin:$PATH"
 
 RUN pip install --no-cache-dir -r requirements.txt
 
--- FrontEnd/FrE.py
+++ FrontEnd/FrE.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 
 app = Flask("__name__")
 
 @app.route('/')
-def hello():
-    return "0"
+def renderThisPath():
+    res = render_template('template-file.html')
+    return res
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    age = request.form['age']
+    alcohol = request.form['alcohol']
+    diet = request.form['diet']
+    phys_act = request.form['phys_act']
+    smoking = request.form['smoking']
+    gender = request.form['gender']
+    lastAppoint = request.form['lastAppoint']
+
+    return f"""
+        Ηλικία: {age}<br>
+        Αλκοόλ: {alcohol}<br>
+        Δίαιτα: {diet}<br>
+        Σωματική Δραστηριότητα: {phys_act}<br>
+        Καπνίσμα: {smoking}<br>
+        Φύλο: {gender}<br>
+        Τελευταία Επίσκεψη: {lastAppoint}
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
