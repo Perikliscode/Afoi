c56f97bb16c8fabc41c5a0c070926e4a4582ea83
Perikliscode <ppantela@csd.auth.gr>
2025-02-18 11:13:29 +0200

    Health assistant, prognosis function

---
+++ BackEnd/BkE.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="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")
+
+def prognosis(age,alcohol,diet,phys_act,smoking,gender,lastAppoint):
+    promt="  What is the percentage of having cancer based on these life factors age:50,alcohol:everyday,diet:bad. What can i do to lower the chances(advice).Also give a random fact that increases cancer chacne and people often neglet"
+
+    with open("BackEnd/AppointmentDoc.txt", "r") as file:
+        file_contentAppoint = file.read()
+    with open("BackEnd/data.txt", "w+") as file:
+        file.write("User Cancer risk factors:\n")
+        file.write("age="+str(age)+"\n")
+        file.write("alcohol="+str(alcohol)+" (0=none,1=little,2=regularly,3=everyday)\n")
+        file.write("diet="+str(diet)+" (0=excelent,1=okay,2=bad,3=very bad)\n")
+        file.write("phys_act="+str(phys_act)+" (0=everyday,1=regularly,2=little,3=none)\n")
+        file.write("smoking="+str(smoking)+" (0=none,1=little,2=regularly,3=everyday)\n")
+        file.write("gender="+gender+"\n")
+        file.write("Last appointment:"+str(lastAppoint))
+        file.seek(0)
+        file_contentData = file.read()
+
+    promt=f"""
+    You are a health assistant that helps users of an app and calculates an approximation on risk of cancer based on their given factors
+    at the end  tell them an approximation of concerning using words
+    tell them that they sould visit a doctor soon and the type of doctor they need.
+    What can i do to lower the chances(advice).
+    Based on the last appointment suggest an exact date when should the next one be.
+    Also give a fact that increases cancer chance and people often neglet.
+    Now, based on the folloing information given, please answer (Do not repeat the information the user gave)
+    {file_contentData}
+    {file_contentAppoint}
+    """
+
+    response = client.chat.completions.create(messages=[{"role":"user","content":promt}],model="gpt-4o-mini",temperature=0.0,max_tokens=1000)
+
+    response = response.choices[0].message.content
+
+    return response
+
--- FrontEnd/FrE.py
+++ FrontEnd/FrE.py
@@ -5,7 +5,7 @@ app = Flask("__name__")
 
 @app.route('/')
 def hello():
-    return "0"
+    return "potato"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
